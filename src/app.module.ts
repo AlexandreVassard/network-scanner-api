51def1cf97978c2d018efbf7217de3182ef3dcab
Alexandre Vassard <alexandre.vassard@bibliosansfrontieres.org>
2023-11-15 16:25:06 +0100

    refactor: remove useless files, add ci

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { NetworkScannerModule } from './network-scanner/network-scanner.module';
 import { DevicesModule } from './devices/devices.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -20,7 +18,5 @@ import { configSchema } from './config/schema';
     NetworkScannerModule,
     DevicesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
